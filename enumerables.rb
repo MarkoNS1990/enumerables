5af297334d02d110e157c738c45c2ee546e885cd
Deepakdanger <depk.96.kumar@gmail.com>
2021-03-19 13:22:17 +0530

    add each with index

--- enumerables.rb
+++ enumerables.rb
@@ -6,7 +6,17 @@ module Enumerable
             i+=1
         end
     end
+
+    def my_each_with_index
+        i = 0
+        while i < self.length
+            yield self[i], i
+            i += 1
+        end
+    end
 end
 
 #my_each
 ['a','b',3,'d'].my_each{|i| puts i}
+#my_each_with_index
+['a','b',3,'d'].my_each_with_index{|i, index| p i, index}
