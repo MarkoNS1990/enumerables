46e661ec3859165ad8106525cbd19e4248283118
Marko Zecevic <marczecevic@gmail.com>
2021-03-19 09:16:17 +0100

    added my_select method

--- enumerables.rb
+++ enumerables.rb
@@ -14,9 +14,22 @@ module Enumerable
             i += 1
         end
     end
+
+    def my_select
+        newArr = []
+        i=0
+        while i < self.length
+            newArr << self[i] if yield  self[i]
+                i+=1
+        end
+     newArr
+    end
 end
 
 #my_each
 ['a','b',3,'d'].my_each{|i| puts i}
 #my_each_with_index
 ['a','b',3,'d'].my_each_with_index{|i, index| p i, index}
+#my_select
+myArray = [1,3,4,10,8].my_select {|num| num > 3}  #[4,10,8]
+p myArray
