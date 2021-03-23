201625853593758b4cf0f9d1cc6a6745b4309fa4
Deepakdanger <depk.96.kumar@gmail.com>
2021-03-23 13:52:01 +0530

    raarrange the method

--- enumerables.rb
+++ enumerables.rb
@@ -1,103 +1,100 @@
 module Enumerable
-    def my_each
-        i = 0  #iterator
-        while  i < self.length
-            yield self[i]
-            i+=1
-        end
+  def my_each
+    i = 0  # iterator
+    while  i < self.length
+        yield self[i]
+        i+=1
     end
+  end
 
-    def my_each_with_index
-        i = 0
-        while i < self.length
-            yield self[i], i
-            i += 1
-        end
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield self[i], i
+      i += 1
     end
+  end
 
-    def my_select
-        newArr = []
-        i=0
-        while i < self.length
-            newArr << self[i] if yield  self[i]
-                i+=1
-        end
-     newArr
+  def my_select
+    newArr = []
+    i=0
+    while i < self.length
+      newArr << self[i] if yield  self[i]
+      i+=1
     end
+    newArr
+  end
 
-    def my_all?
-        self.my_each do |num|
-            return false unless yield (num)
-        end
-        return true        
+  def my_all?
+    self.my_each do |num|
+      return false unless yield (num)
     end
+    return true        
+  end
 
-    def my_any?
-        variable = 0
-        self.my_each do |num|
-            variable+=1 if yield num           
-        end
-        puts true if variable > 0
-        puts false if variable == 0
+  def my_any?
+    variable = 0
+    self.my_each do |num|
+      variable+=1 if yield num           
     end
-
-    def my_none?
-        self.my_each do |num|
-            return false if yield (num)
-        end
-        return true  
-
+    puts true if variable > 0
+    puts false if variable == 0
+  end
+  
+  def my_none?
+    self.my_each do |num|
+      return false if yield (num)
     end
+    return true
+  end
 
-    def my_count
-        arr=[]
-        self.my_each do |num|
-            arr << num if yield (num)
-        end
-        return arr.length
+  def my_count
+    arr=[]
+    self.my_each do |num|
+      arr << num if yield (num)
     end
+    return arr.length
+  end
 
-    def my_map(proc1)
-        newArr = []
-        if block_given?
-            self.my_each do |el|
-              newArr << yield(el)
-            end
-
-        else
-            self.my_each do |el|
-                newArr << proc1.call(el)
-            end
-        end
-
-        newArr
+  def my_map(proc1)
+    newArr = []
+    if block_given?
+      self.my_each do |el|
+        newArr << yield(el)
+      end
+    else
+      self.my_each do |el|
+        newArr << proc1.call(el)
+      end
     end
+    newArr
+  end
 
-    def my_inject
-        result=self[0] # Start value for result
-         for i in 1...self.length #Iterate through array
-             result = yield(result, self[i]) #Result gets new value - result=result + current num
-            end
-         result
-     end
+  def my_inject
+    result=self[0] # Start value for result
+    for i in 1...self.length # Iterate through array
+      result = yield(result, self[i]) # Result gets new value - result=result + current num
+    end
+    result
+  end
 end
 
 def multiply_els(arr)
-    arr.my_inject {|a,b| a*b}
+  arr.my_inject {|a,b| a*b}
 end
 
-#my_each
+# my_each
 # ['a','b',3,'d'].my_each{|i| puts i}
-#my_each_with_index
+# my_each_with_index
 # ['a','b',3,'d'].my_each_with_index{|i, index| p i, index}
-#my_select
+# my_select
 # myArray = [1,3,4,10,8].my_select {|num| num > 3}  #[4,10,8]
 # p myArray
-#my_all
+# my_all
 # [12,16,17,18,19].my_all? {|num| puts num > 10}  #true
-#my_any
+# my_any
 # [12,16,17,18].my_any? {|num| num > 20}  #False
-#my_none
+# my_none
 # variable =[12,16,17,18].my_none? {|num| num > 17} # 
 # puts variable
 # my_count
