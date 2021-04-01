a952cbab1ce95ef7e4ade58a202b9e7adb6c06a5
Marko Zecevic <marczecevic@gmail.com>
2021-04-01 11:24:14 +0200

    Add testing patterns for my_all method

--- spec/data_type_test.rb
+++ spec/data_type_test.rb
@@ -17,29 +17,15 @@ describe '#my_select' do
     expect([1, 2, 3].my_select.instance_of?(Enumerator)).to eql(true)
   end
 
-describe '#my_select' do
-  let(:array) { Array.new(100) { rand(0...9) } }
-  let(:words) { %w[dog door rod blade] }  
-  
-  it 'should return true if all elements are of Integer type' do
-    expect(array.my_all?(Integer) == array.all?(Integer)).to eql(true)
+  describe '#my_select' do
+    let(:array) { Array.new(100) { rand(0...9) } }
+
+    it 'should return true if all elements are of Integer type' do
+      expect(array.my_all?(Integer) == array.all?(Integer)).to eql(true)
+    end
+
+    it 'should return true if all elements are of Numeric type' do
+      expect(array.my_all?(Numeric) == array.all?(Numeric)).to eql(true)
+    end
   end
-
-  it 'should return true if all elements are of Numeric type' do
-    expect(array.my_all?(Numeric) == array.all?(Numeric)).to eql(true)
-  end
-
-  it 'regex1' do 
-    
-    expect(words.my_all?(/d/) == words.all?(/d/)).to eql(true)
-  end
-
-  it 'regex2' do 
-    
-    expect(words.my_all?(/o/) == words.all?(/o/)).to eql(true)
-  end
-
-
-end
-
 end
--- spec/my_all_test.rb
+++ spec/my_all_test.rb
@@ -6,8 +6,8 @@ describe '#my_all' do
   let(:false_block) { proc { |num| num > 9 } }
   let(:words) { %w[dog door rod blade] }
   let(:array) { Array.new(100) { rand(0...9) } }
-  let(:true_array) {[1, true, 'hi', []]}
-  let(:false_array) {[1, false, 'hi', []]}
+  let(:true_array) { [1, true, 'hi', []] }
+  let(:false_array) { [1, false, 'hi', []] }
 
   it 'should return true if block never returns false or nil' do
     expect(array.my_all?(&true_block) == array.all?(&true_block)).to eql(true)
@@ -21,12 +21,25 @@ describe '#my_all' do
     expect(range.my_all?(&false_block) == range.all?(&false_block)).to eql(true)
   end
 
-  it 'should return true if all elements of an array are true' do 
+  it 'should return true if all elements of an array are true' do
     expect(true_array.my_all? == true_array.all?).to eql(true)
   end
 
-  it 'should return true if any of elements of an array are false' do 
+  it 'should return true if any of elements of an array are false' do
     expect(false_array.my_all? == false_array.all?).to eql(true)
   end
 
+  it 'must return true when regex is passed as an argument all of the collection matches the regex' do
+    expect(words.my_all?(/d/) == words.all?(/d/)).to eql(true)
+  end
+
+  it 'must return true when regex is passed as an argument all of the collection does not match the regex' do
+    expect(words.my_all?(/o/) == words.all?(/o/)).to eql(true)
+  end
+
+  it 'when pattern other than regex or class is given return true if all of the collection matches the pattern' do
+    array = []
+    10.times { array << 3 }
+    expect(array.my_all?(3) == array.all?(3)).to eql(true)
+  end
 end
