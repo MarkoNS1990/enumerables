483f3bc964d94afc1ac94c4a4894ad8e40f5a979
Marko Zecevic <marczecevic@gmail.com>
2021-04-01 11:12:56 +0200

    Add check for different types for my_all

--- spec/data_type_test.rb
+++ spec/data_type_test.rb
@@ -16,4 +16,30 @@ describe '#my_select' do
   it 'should return true if it is an Enumerable' do
     expect([1, 2, 3].my_select.instance_of?(Enumerator)).to eql(true)
   end
+
+describe '#my_select' do
+  let(:array) { Array.new(100) { rand(0...9) } }
+  let(:words) { %w[dog door rod blade] }  
+  
+  it 'should return true if all elements are of Integer type' do
+    expect(array.my_all?(Integer) == array.all?(Integer)).to eql(true)
+  end
+
+  it 'should return true if all elements are of Numeric type' do
+    expect(array.my_all?(Numeric) == array.all?(Numeric)).to eql(true)
+  end
+
+  it 'regex1' do 
+    
+    expect(words.my_all?(/d/) == words.all?(/d/)).to eql(true)
+  end
+
+  it 'regex2' do 
+    
+    expect(words.my_all?(/o/) == words.all?(/o/)).to eql(true)
+  end
+
+
+end
+
 end
--- spec/my_all_test.rb
+++ spec/my_all_test.rb
@@ -6,6 +6,8 @@ describe '#my_all' do
   let(:false_block) { proc { |num| num > 9 } }
   let(:words) { %w[dog door rod blade] }
   let(:array) { Array.new(100) { rand(0...9) } }
+  let(:true_array) {[1, true, 'hi', []]}
+  let(:false_array) {[1, false, 'hi', []]}
 
   it 'should return true if block never returns false or nil' do
     expect(array.my_all?(&true_block) == array.all?(&true_block)).to eql(true)
@@ -18,4 +20,13 @@ describe '#my_all' do
   it 'should return true if block returns false or nil:range ' do
     expect(range.my_all?(&false_block) == range.all?(&false_block)).to eql(true)
   end
+
+  it 'should return true if all elements of an array are true' do 
+    expect(true_array.my_all? == true_array.all?).to eql(true)
+  end
+
+  it 'should return true if any of elements of an array are false' do 
+    expect(false_array.my_all? == false_array.all?).to eql(true)
+  end
+
 end
